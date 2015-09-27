1dc1264fe4f3b76aef57d2adf4c0107d8f536e8d
elit69 <chhornelit@gmail.com>
2015-09-27 11:21:24 +0700

    ''

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,12 @@
 class TweetsController < ApplicationController
   def index 
-
+    @tweets = Tweet.all
   end
   def show
     @tweet = Tweet.find(params[:id])
+  end
+  def destroy
+    Tweet.find(params[:id]).destroy
+    redirect_to action: :index
   end
 end
