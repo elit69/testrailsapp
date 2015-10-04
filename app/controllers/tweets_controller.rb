af522c50ef5bc42a63db77c46f33edea1d953565
elit69 <chhornelit@gmail.com>
2015-10-04 10:35:59 +0700

    asdf

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,21 +12,28 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @tweet }
-      format.xml { render xml: @tweet }
+      format.json { render json: @tweet , except: [:updated_at, :created_at]  }
+      format.xml { render xml: @tweet , except: [:updated_at, :created_at]  }
     end
   end
-
   def new 
     @tweet = Tweet.new
   end
-
+  def create
+    @tweet = Tweet.create(tweet_params)
+    @tweet.save
+    redirect_to @tweet
+  end
   def destroy
     Tweet.find(params[:id]).destroy
     redirect_to action: :index
   end
-
-  #custom routes
+  
+  ##########custom function
+  def tweet_params
+    params.require(:tweet).permit([:status, :zombie_id])
+  end  
+  #######custom routes
   #def status
   #  render action: :statuss
   #end
